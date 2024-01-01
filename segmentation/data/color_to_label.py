3609c5fd3ba6f59208fdc4c8eb1d7045ea8a1af2
Fatih Haslak <haslak749@gmail.com>
2024-01-01 17:43:04 +0300

    yorum satırı eklendi

--- segmentation/data/color_to_label.py
+++ segmentation/data/color_to_label.py
@@ -8,12 +8,14 @@ class Color_to_label_map(nn.Module):
         self.human_colors = [(60, 20, 220), (0, 0, 255)]
         
     
-    def forward(self,x):#RGB İMAGE
-            # Görüntüyü düzenle
+    def forward(self,x):#image 
+
+            #siyas maske oluştur
             self.zeros_mask=np.zeros(x.shape[:2])
+            #pixel değerlerine göre 1 ve 2 diye etiket ata
             mask_car = np.all(x == np.array(self.car_color), axis=2)
             mask_human = np.any(np.all(x[:, :, None, :] == np.array(self.human_colors)[None, None, :, :], axis=3), axis=2)
-
+            #maskelemeyi yaparak label_map olustur
             self.zeros_mask[mask_car] = 1
             self.zeros_mask[mask_human] = 2
 
